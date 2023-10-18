1281c536032582e837dde292c42e697be3ef83c1
Sneha Deshpande <snehajoshi1895@gmail.com>
2023-10-18 17:30:16 +0530

    updated

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.use('uploads', express.static('uploads'));
 // }).
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'uploads/'); // The directory where uploaded files will be stored
+      cb(null, 'images/'); // The directory where uploaded files will be stored
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + '-' + file.originalname); // Rename the file
@@ -96,7 +96,7 @@ const storage = multer.diskStorage({
 
   const upload = multer({ storage: storage });  
 
-  app.use('/uploads', express.static('uploads'));
+  app.use('/uploads', express.static('images'));
 
 app.get('/', (req, res) => {
     // Handle the GET request
