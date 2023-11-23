285892f53e9ebca983fb372baef9c90ac0df25af
Sneha Deshpande <snehajoshi1895@gmail.com>
2023-11-23 16:42:18 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 
 const URL = process.env.URL;
@@ -11,16 +11,16 @@ const CorsOptions = {
     origin: `${URL}`,
     optionSuccessStatus: 200
 }
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://samadhan-legal-services.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', 'https://samadhan-legal-services.netlify.app');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     next();
+// });
 
 app.use(express.json());
 app.use(cors(CorsOptions));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 app.use('/uploads', express.static('uploads'));
 
 const adminRoute = require('./Routes/Admin');
@@ -29,7 +29,6 @@ const lawyerRoute = require('./Routes/Lawyer');
 const profileRoute = require('./Routes/Profile');
 const signInRoute = require('./Routes/Signin');
 
-// app.use(indexRoute);
 app.use('/adminRoute',adminRoute);
 app.use('/clientRoute',clientRoute);
 app.use('/lawyerRoute',lawyerRoute);
