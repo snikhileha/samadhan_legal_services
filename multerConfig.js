e98f702882eaab48da61efc121a5371c0db29927
Sneha Deshpande <snehajoshi1895@gmail.com>
2023-11-24 14:42:51 +0530

    updated

--- multerConfig.js
+++ multerConfig.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path');
 
-
+// console.log(path.join(__dirname,'../uploads'));
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads'); 
+        cb(null, path.join(__dirname,'../uploads')); 
     },
     filename: function (req, file, cb) {
       cb(null, Date.now() + '-' + file.originalname); 
